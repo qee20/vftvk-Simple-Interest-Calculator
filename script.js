212e7154318e9b8b303e8a577985eb11b88613bc
qee20 <ricqee@outlook.com>
2022-07-20 18:48:31 +0700

    Fix web , add component to html, add some styling to css and some functions and variables to javascript

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+
+
+// textbox changong value
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+    //variables
+
+    var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+
+// checking principal value function
+
+if (principal < 0 || principal==0) {
+    alert("Enter a positive number")
+    document.getElementById("principal").focus();   
+}
+else{
+    document.getElementById("result").innerHTML=`If you deposit ${principal} , at an interest rate of ${rate}. You will receive an amount of ${interest}, in the year ${year}`
+}
 }
         
